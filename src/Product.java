910c4202f1a6e86ea24f6e463bce574722cd477d
Antonella Solomon <antonellasolomon@gmail.com>
2018-02-20 22:42:45 -0500

    made boolean in product private

--- src/Product.java
+++ src/Product.java
@@ -3,7 +3,7 @@ public abstract class Product {
 	private String name;
 	private double price;
 	private int quantity;
-	boolean imported;
+	private boolean imported;
 
 	public Product(String name, double price) {
 		super();
